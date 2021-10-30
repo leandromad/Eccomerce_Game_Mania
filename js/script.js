91774b938eadaccf97ae3b1e2a37692f143ccaf6
leandromad <leandro.palha.madureira@gmail.com>
2021-10-30 10:20:01 -0300

    Adiciona a pasta de scripts.

---
+++ js/script.js
@@ -0,0 +1,78 @@
+$(document).ready(function () {
+});
+
+let email = document.getElementById('campo-email');
+
+function enviarEmail() {
+    let emailDigitado = email.value;
+    console.log(emailDigitado);
+}
+
+
+
+function renderizaProdutos() {
+    let lista = document.getElementById('espacoProdutos');
+
+    let listaProdutos = [
+        {
+            Imagem: 'assets/image 1.jpg',
+            nome: 'FIFA 2022',
+            categoria: 'Jogos',
+            marca: 'EA Sports',
+            preço: 'R$ 200,00'
+        },
+    
+        {
+            Imagem: 'assets/image 3.png',
+            nome: 'Iphone Apple 13',
+            categoria: 'SmartPhone',
+            marca: 'Apple',
+            preço: 'R$ 8.000,00'
+        },
+        {
+            Imagem: 'assets/image 4.png',
+            nome: 'Xbox Series X',
+            categoria: 'Console',
+            marca: 'Microsoft',
+            preço: 'R$ 7.000,00'
+        }
+        
+    ]
+
+    let template = '';
+
+    for (let i = 0; i < listaProdutos.length; i++) {
+        const produto = listaProdutos[i];
+
+        template += `<div class="product">
+        <a href="#">
+          <img src="${produto.Imagem}" alt="">
+          <h5>${produto.nome}</h5>
+          <p>${produto.categoria}</p>
+        </a>
+        <span class="rate"><i>★</i><i>★</i><i>★</i><i>★</i><i>★</i></span>
+        <a href="#">
+          <p class="marca">${produto.marca}</p>
+          <h4>${produto.preço}</h4>
+        </a>
+      </div>`
+    }
+
+    lista = lista.innerHTML = template;
+}
+
+const swiper = new Swiper('.swiper', {
+    slidesPerView: 1,
+    loop: true,
+    pagination: {
+        el: '.swiper-pagination'
+    },
+    mousewheel: true,
+    keyboard: true,
+     // Navigation arrows
+    navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  }
+
+})
